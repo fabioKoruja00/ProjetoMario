1d9e7f78402e1acbd53b76c62cbab9fd08c6f303
fabioKoruja00 <122614728+fabioKoruja00@users.noreply.github.com>
2023-02-03 22:26:36 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-const botaoTrailer = document.querySelector(".botao-piscando");
+const botaoTrailer = document.querySelector(".botao-trailer");
 const botaoFecharModal = document.querySelector(".fechar-modal");
 const video = document.getElementById("video");
 const modal = document.querySelector(".modal");
